9e0b91a7537614c8b9d3f9221b2a86a4b9317234
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-01-16 00:36:19 -0800

    Semistandard

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -14,13 +14,13 @@ $(document).ready(function () {
     $('.amenities h4').text(Object.values(selectedAmenities).join(', '));
   });
 
-    // getting the status of API
+  // getting the status of API
 
-    $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
-      if (data.status === "OK") {
-        $("div#api_status").addClass("available");
-      } else {
-        $("div#api_status").removeClass("available");
-      }
-    });
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  });
 });
--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -14,26 +14,26 @@ $(document).ready(function () {
     $('.amenities h4').text(Object.values(selectedAmenities).join(', '));
   });
 
-    // getting the status of API
+  // getting the status of API
 
-    $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
-      if (data.status === "OK") {
-        $("div#api_status").addClass("available");
-      } else {
-        $("div#api_status").removeClass("available");
-      }
-    });
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  });
 
-    // fetching data about places
-    $.post({
-      url: 'http://0.0.0.0:5001/api/v1/places_search',
-      data: JSON.stringify({}),
-      headers: {
-        "Content-Type": "application/json",
-      },
-      success: (data) => {
-        data.forEach((place) =>
-          $("section.places").append(
+  // fetching data about places
+  $.post({
+    url: 'http://0.0.0.0:5001/api/v1/places_search',
+    data: JSON.stringify({}),
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    success: (data) => {
+      data.forEach((place) =>
+        $('section.places').append(
             `<article>
         <div class="title_box">
               <h2>${place.name}</h2>
@@ -41,22 +41,22 @@ $(document).ready(function () {
               </div>
               <div class="information">
         <div class="max_guest">${place.max_guest} Guest${
-          place.max_guest !== 1 ? "s" : ""
+          place.max_guest !== 1 ? 's' : ''
             }</div>
                     <div class="number_rooms">${place.number_rooms} Bedroom${
-          place.number_rooms !== 1 ? "s" : ""
+          place.number_rooms !== 1 ? 's' : ''
             }</div>
         <div class="number_bathrooms">${place.number_bathrooms} Bathroom${
-          place.number_bathrooms !== 1 ? "s" : ""
+          place.number_bathrooms !== 1 ? 's' : ''
             }</div>
               </div>
         <div class="description">
         ${place.description}
               </div>
             </article>`
-          )
-        );
-      },
-      dataType: "json",
-    });
+        )
+      );
+    },
+    dataType: 'json'
+  });
 });
